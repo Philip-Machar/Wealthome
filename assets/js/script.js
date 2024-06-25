a93c372a0706216950226658ce7555de853c1150
Philip-Machar <philipmachar788@gmail.com>
2024-06-25 09:33:02 +0300

    header section mobile js complete

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/**
+* navbar toggle in mobile
+*/
+
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+
+/**
+* scroll state
+*/
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+
+
+
